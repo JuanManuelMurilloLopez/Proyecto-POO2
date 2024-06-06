196479ad21636f717583f5a51550b1dc3a219225
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2024-06-05 22:01:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,32 +4,356 @@
 #include"Servicio.h"
 
 int main(){
+    //Recuperamos la fecha actual del sistema
     auto fecha = std::chrono::system_clock::now();
-    unsigned int opcion = 1;
+    unsigned int opcion;
+    //Creamos el objeto de servicio
     Servicio servicio(fecha);
-    servicio.mostrarFecha();
-    while (opcion != 0)
-    {
+
+    //Menú ciclado
+    do{
         std::cout<<"\nMENU\n";
+        //Opciones de funcionalidades para el usuario
         std::cout<<"1. Añadir Vehiculo\n";
         std::cout<<"2. Consultar datos de Vehiculo\n";
-        std::cout<<"3. Información\n";
+        std::cout<<"3. Información del servicio\n";
         std::cout<<"4. Cargar ejemplos predeterminados\n";
+        std::cout<<"0. SALIR\n";
         std::cout<<"Ingresa la opción: \n";
-        std::cin>>opcion;
+        std::cin>>opcion; 
         if(opcion == 1){
-            unsigned int tipo = 0;
-            std::cout<<"1. Auto Gasolina\n";
-            std::cout<<"2. Auto Eléctrico\n";
+            unsigned int tipo;
+            std::cout<<"1. Auto Eléctrico\n";
+            std::cout<<"2. Auto Gasolina\n";
             std::cout<<"3. Van\n";
             std::cout<<"4. Moto\n";
+            std::cin>>tipo;
             if(tipo == 1){
+                //Pedimos los datos del Auto Eléctrico al usuario
 
-                //servicio.agregarVehiculo();
-                //Después de pedir todos los atributos, crear el objeto dentro de la clase servicio.
+                //Atributos Generales de clase Vehículo
+                int ID;
+                std::cout<<"Ingrese el ID del Vehiculo: ";
+                std::cin>>ID;
+                int kilometraje;
+                std::cout<<"Ingrese el kilometraje: ";
+                std::cin>>kilometraje;
+                int anio;
+                std::cout<<"Ingrese el año: ";
+                std::cin>>anio;
+                float tiempoAnteriorServicio;
+                std::cout<<"Ingrese el tiempo desde el anterior servicio (meses): ";
+                std::cin>>tiempoAnteriorServicio;
+                float estadoLlantas;
+                std::cout<<"Ingrese el estado de llantas (porcentaje): ";
+                std::cin>>estadoLlantas;
+                bool garantia;
+                std::cout<<"El Vehiculo tiene garantia? (Si->1, No->0): ";
+                std::cin>>garantia;
+                std::string tipoTransmision;
+                int opTransmision;
+                std::cout<<"Ingrese la opción de tipo de transmision";
+                std::cout<<"1. Manual";
+                std::cout<<"2. Automática";
+                std::cin>>opTransmision;
+                if(opTransmision == 1){
+                    tipoTransmision = "Manual";
+                }
+                if(opTransmision == 2){
+                    tipoTransmision = "Automática";
+                }
+                std::string tipoFrenos;
+                int opFrenos;
+                std::cout<<"Ingrese la opción del tipo de Frenos";
+                std::cout<<"1. Disco";
+                std::cout<<"2. Tambor";
+                std::cin>>opFrenos;
+                if(opFrenos == 1){
+                    tipoFrenos = "Disco";
+                }
+                if(opFrenos == 2){
+                    tipoFrenos = "Tambor";
+                }
+
+                //Atributos específicos de la clase Auto
+                int numeroPuertas;
+                std::cout<<"Ingrese el numero de puertas: ";
+                std::cin>>numeroPuertas;
+                std::string carroceria;
+                int opCarroceria;
+                std::cout<<"Ingrese la opción del tipo de carrocería: ";
+                //Hatckback, Sedán, Coupe
+                std::cout<<"1. Hatchback";
+                std::cout<<"2. Sedán";
+                std::cout<<"3. Coupe";
+                std::cin>>opCarroceria;
+                if(opCarroceria == 1){
+                    carroceria = "Hatchback";
+                }
+                if(opCarroceria == 2){
+                    carroceria = "Sedán";
+                }
+                if(opCarroceria == 3){
+                    carroceria = "Coupe";
+                }
+
+                //Atributos específicos de la clase AutoEletrico
+                float capacidadBateria;
+                std::cout<<"Ingrese la capacidad de la batería";
+                std::cin>>capacidadBateria;
+
+                //Creamos el objeto con los valores recibidos
+                servicio.crearAutoElectrico(ID,anio,kilometraje,tiempoAnteriorServicio,estadoLlantas,garantia,tipoTransmision,tipoFrenos,numeroPuertas,carroceria,capacidadBateria);
+
+            } 
+            if(tipo == 2){
+                //Pedimos los datos del Auto de Gasolina al usuario
+
+                //Atributos Generales de clase Vehículo
+                int ID;
+                std::cout<<"Ingrese el ID del Vehiculo: ";
+                std::cin>>ID;
+                int anio;
+                std::cout<<"Ingrese el año: ";
+                std::cin>>anio;
+                int kilometraje;
+                std::cout<<"Ingrese el kilometraje: ";
+                std::cin>>kilometraje;
+                float tiempoAnteriorServicio;
+                std::cout<<"Ingrese el tiempo desde el anterior servicio (meses): ";
+                std::cin>>tiempoAnteriorServicio;
+                float estadoLlantas;
+                std::cout<<"Ingrese el estado de llantas (porcentaje): ";
+                std::cin>>estadoLlantas;
+                bool garantia;
+                std::cout<<"El Vehiculo tiene garantia? (Si->1, No->0): ";
+                std::cin>>garantia;
+                std::string tipoTransmision;
+                int opTransmision;
+                std::cout<<"Ingrese la opción de tipo de transmision";
+                std::cout<<"1. Manual";
+                std::cout<<"2. Automática";
+                std::cin>>opTransmision;
+                if(opTransmision == 1){
+                    tipoTransmision = "Manual";
+                }
+                if(opTransmision == 2){
+                    tipoTransmision = "Automática";
+                }
+                std::string tipoFrenos;
+                int opFrenos;
+                std::cout<<"Ingrese la opción del tipo de Frenos";
+                std::cout<<"1. Disco";
+                std::cout<<"2. Cambor";
+                std::cin>>opFrenos;
+                if(opFrenos == 1){
+                    tipoFrenos = "Disco";
+                }
+                if(opFrenos == 2){
+                    tipoFrenos = "Tambor";
+                }
+
+                //Atributos específicos de la clase Auto
+                int numeroPuertas;
+                std::cout<<"Ingrese el numero de puertas: ";
+                std::cin>>numeroPuertas;
+                std::string carroceria;
+                int opCarroceria;
+                std::cout<<"Ingrese la opción del tipo de carrocería: ";
+                //Hatckback, Sedán, Coupe
+                std::cout<<"1. Hatchback";
+                std::cout<<"2. Sedán";
+                std::cout<<"3. Coupe";
+                std::cin>>opCarroceria;
+                if(opCarroceria == 1){
+                    carroceria = "Hatchback";
+                }
+                if(opCarroceria == 2){
+                    carroceria = "Sedán";
+                }
+                if(opCarroceria == 3){
+                    carroceria = "Coupe";
+                }
+
+                //Atributos específicos de la clase Auo Gasolina
+                float cilindraje;
+                std::cout<<"Ingrese el cilindraje: ";
+                std::cin>>cilindraje;
+                int numeroCilindros;
+                std::cout<<"Ingrese el número de cilindros: ";
+                std::cin>>numeroCilindros;
+                std::string tipoAceite;
+                int opAceite;
+                std::cout<<"Ingrese la opcion del tipo de Aceite: ";
+                //Mineral, Semi-Sintetico, Sintetico
+                std::cout<<"1. Mineral";
+                std::cout<<"2. Semi-Sintetico";
+                std::cout<<"3. Sintético";
+                std::cin>>opAceite;
+                if(opAceite == 1){
+                    tipoAceite = "Mineral";
+                }
+                if(opAceite == 2){
+                    tipoAceite = "Semi-Sintético";
+                }
+                if(opAceite == 3){
+                    tipoAceite = "Sintético";
+                }
+
+                //Creamos el objeto con los valores recibidos
+                servicio.crearAutoGasolina(ID,anio,kilometraje,tiempoAnteriorServicio,estadoLlantas,garantia,tipoTransmision,tipoFrenos,numeroPuertas,carroceria,cilindraje,numeroCilindros,tipoAceite);
+            }
+            if(tipo == 3){
+                //Pedimos los datos de la Van al usuario
+
+                //Atributos Generales de clase Vehículo
+                int ID;
+                std::cout<<"Ingrese el ID del Vehiculo: ";
+                std::cin>>ID;
+                int kilometraje;
+                std::cout<<"Ingrese el kilometraje: ";
+                std::cin>>kilometraje;
+                int anio;
+                std::cout<<"Ingrese el año: ";
+                std::cin>>anio;
+                float tiempoAnteriorServicio;
+                std::cout<<"Ingrese el tiempo desde el anterior servicio (meses): ";
+                std::cin>>tiempoAnteriorServicio;
+                float estadoLlantas;
+                std::cout<<"Ingrese el estado de llantas (porcentaje): ";
+                std::cin>>estadoLlantas;
+                bool garantia;
+                std::cout<<"El Vehiculo tiene garantia? (Si->1, No->0): ";
+                std::cin>>garantia;
+                std::string tipoTransmision;
+                int opTransmision;
+                std::cout<<"Ingrese la opción de tipo de transmision";
+                std::cout<<"1. Manual";
+                std::cout<<"2. Automática";
+                std::cin>>opTransmision;
+                if(opTransmision == 1){
+                    tipoTransmision = "Manual";
+                }
+                if(opTransmision == 2){
+                    tipoTransmision = "Automática";
+                }
+                std::string tipoFrenos;
+                int opFrenos;
+                std::cout<<"Ingrese la opción del tipo de Frenos";
+                std::cout<<"1. Disco";
+                std::cout<<"2. Tambor";
+                std::cin>>opFrenos;
+                if(opFrenos == 1){
+                    tipoFrenos = "Disco";
+                }
+                if(opFrenos == 2){
+                    tipoFrenos = "Tambor";
+                }
+
+                //Atributos específicos de la clase Van
+                int numeroAsientos;
+                std::cout<<"Ingrese el núumero de asientos: ";
+                std::cin>>numeroAsientos;
+                std::string tipoCombustible;
+                int opCombustible;
+                std::cout<<"Ingrese la opción del tipo de combustible: ";
+                std::cout<<"1. Gasolina";
+                std::cout<<"2. Diesel";
+                std::cin>>opCombustible;
+                if(opCombustible == 1){
+                    tipoCombustible = "Gasolina";
+                }
+                if(opCombustible == 2){
+                    tipoCombustible = "Diesel";
+                }
                 
-            };
-        };
+                //Creamos el objeto con los valores recibidos
+                servicio.crearVan(ID,anio,kilometraje,tiempoAnteriorServicio,estadoLlantas,garantia,tipoTransmision,tipoFrenos,numeroAsientos,tipoCombustible);
+            }
+            if(tipo == 4){
+                //Pedimos los datos de la Moto al usuario
+
+                //Atributos Generales de clase Vehículo
+                int ID;
+                std::cout<<"Ingrese el ID del Vehiculo: ";
+                std::cin>>ID;
+                int kilometraje;
+                std::cout<<"Ingrese el kilometraje: ";
+                std::cin>>kilometraje;
+                int anio;
+                std::cout<<"Ingrese el año: ";
+                std::cin>>anio;
+                float tiempoAnteriorServicio;
+                std::cout<<"Ingrese el tiempo desde el anterior servicio (meses): ";
+                std::cin>>tiempoAnteriorServicio;
+                float estadoLlantas;
+                std::cout<<"Ingrese el estado de llantas (porcentaje): ";
+                std::cin>>estadoLlantas;
+                bool garantia;
+                std::cout<<"El Vehiculo tiene garantia? (Si->1, No->0): ";
+                std::cin>>garantia;
+                std::string tipoTransmision;
+                int opTransmision;
+                std::cout<<"Ingrese la opción de tipo de transmision";
+                std::cout<<"1. Manual";
+                std::cout<<"2. Automática";
+                std::cin>>opTransmision;
+                if(opTransmision == 1){
+                    tipoTransmision = "Manual";
+                }
+                if(opTransmision == 2){
+                    tipoTransmision = "Automática";
+                }
+                std::string tipoFrenos;
+                int opFrenos;
+                std::cout<<"Ingrese la opción del tipo de Frenos";
+                std::cout<<"1. Disco";
+                std::cout<<"2. Tambor";
+                std::cin>>opFrenos;
+                if(opFrenos == 1){
+                    tipoFrenos = "Disco";
+                }
+                if(opFrenos == 2){
+                    tipoFrenos = "Tambor";
+                }
+
+                //Atributos específicos de la clase Moto
+                int cilindrada;
+                std::cout<<"Ingrese la cilindrada: ";
+                std::cin>>cilindrada;
+
+                //Creamos el objeto con los valores recibidos
+                servicio.crearMoto(ID,anio,kilometraje,tiempoAnteriorServicio,estadoLlantas,garantia,tipoTransmision,tipoFrenos,cilindrada);
+            }
+        } 
+        if(opcion == 2){
+            int id;
+            std::cout<<"Ingrese el ID del Vehiculo: ";
+            std::cin>>id;
+            servicio.mostrarVehiculo_byID(id);
+        }
+        if(opcion == 3){
+            servicio.imprimirDatos();
+        }
+        if(opcion == 4){
+            //servicio.agregarAutoElectrico();
+            //servicio.agregarAutoGasolina();
+            //servicio.agregarMoto();
+            //servicio.agregarVan();
+            //servicio.agregarAutoElectrico();
+            //servicio.agregarAutoGasolina();
+            //servicio.agregarMoto();
+            //servicio.agregarVan();
+            //servicio.agregarAutoElectrico();
+            //servicio.agregarAutoGasolina();
+            //servicio.agregarMoto();
+            //servicio.agregarVan();
+        }
+        if(opcion > 4){
+            std::cout<<"\nEntrada Incorrecta.\n";
+        }
     }
+    while(opcion != 0);
     return 0;
 };
+

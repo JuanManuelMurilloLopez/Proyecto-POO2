8300cc513d23873e59d52a1db85ddea13e9476fc
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2024-06-11 22:05:26 -0600

    Update Servicio.h

--- Servicio.h
+++ Servicio.h
@@ -34,82 +34,163 @@ class Servicio {
     float precio;
     //Duración del servicio en horas
     float duracion;
+    unsigned int numeroEmpleados;
 
     //Declaración de constructores, métodos y destructor
     public:
-    //Constructor por default
+    /**
+     * Servicio constructor vacío
+     * Inicializa el objeto con los atributos default
+     * @param
+     * @return
+     */
     Servicio(){
         this -> indice = 0;
+        this -> numeroEmpleados = 0;
     };
-    //Constructor con fecha
+
+    /**
+     * Servicio constructor con parámetro de fecha
+     * Recibe la fecha del usuario
+     * además de inicializar el índice
+     * @param const std::chrono::time_point<std::chrono::system_clock>&
+     * @return
+     */
     Servicio(const std::chrono::time_point<std::chrono::system_clock>& fech){
         this -> fecha = fech;
         this -> indice = 0;
+        this -> numeroEmpleados = 0;
     }
-    ~Servicio(){};
+
     /**
-     * crearAutoElectrico crea un objeto AutoElectrico y lo agrega al arreglo de vehiculos
+     * setNumeroEmpleados setter de atributo NumeroEmpleados
+     * Recibe el NumeroEmpleados y lo guarda en el atributo NumeroEmpleados
+     * @param unsigned int NumeroEmpleados
+     * @return 
+     */
+    void setNumeroEmpleados(int numEmp){
+        this -> numeroEmpleados = numEmp;
+    }
+
+    /**
+     * crearAutoElectrico crea un objeto AutoElectrico y lo agrega al arreglo
+     * de vehiculos
      * 
-     * crea un objeo AutoElectrico y lo agrega al arreglo de empleados utilizando el aributo de índice para la posición
+     * crea un objeo AutoElectrico y lo agrega al arreglo de empleados
+     * utilizando el aributo de índice para la posición
      * incrementándolo después en 1.
      * 
-     * @param 
+     * @param int ID, int año, int kilometraje, float tiempo anterior
+     * servicio, float estado llantas, bool garantia,
+     * string tipo transmisión, string tipo frenos, int numero puertas,
+     * string carrocería, float capacidad batería
      * @return
     */
-    void crearAutoElectrico(int ID, int anio, int kilometraje, float tiempoAnteriorServicio, float estadoLlantas, bool garantia, std::string tipoTransmision, std::string tipoFrenos, int numeroPuertas, std::string carroceria, float capacidadBateria){
-        vehiculos[indice] = new AutoElectrico(ID, anio, kilometraje, tiempoAnteriorServicio, estadoLlantas, garantia, tipoTransmision, tipoFrenos, numeroPuertas, carroceria, capacidadBateria);
+    void crearAutoElectrico(int ID,int an,int km,float tas,float el,bool gar,
+    std::string tt,std::string tf,int np,std::string car,float cb){
+        vehiculos[indice] = new AutoElectrico(ID,an,km,tas,el,gar,tt,
+        tf,np,car,cb);
         indice++;
     };
-        /**
-     * crearAutoGasolina crea un objeto AutoGasolina y lo agrega al arreglo de vehiculos
+
+    /**
+     * crearAutoGasolina crea un objeto AutoGasolina y lo agrega al arreglo
+     * de vehiculos
      * 
-     * crea un objeto AutoGasolina y lo agrega al arreglo de empleados utilizando el aributo de índice para la posición
+     * crea un objeto AutoGasolina y lo agrega al arreglo de empleados
+     * utilizando el aributo de índice para la posición
      * incrementándolo después en 1.
      * 
-     * @param 
+     * @param int ID, int año, int kilometraje, float tiempo anterior
+     * servicio, float estado llantas, bool garantia,
+     * string tipo transmisión, string tipo frenos, int numero puertas,
+     * string carrocería, float cilindraje, int numero cilindros,
+     * string tipo aceite
      * @return
     */
-    void crearAutoGasolina(int ID, int anio, int kilometraje, float tiempoAnteriorServicio, float estadoLlantas, bool garantia, std::string tipoTransmision, std::string tipoFrenos, int numeroPuertas, std::string carroceria, float cilindraje, int numeroCilindros, std::string tipoAceite){
-        vehiculos[indice] = new AutoGasolina(ID, anio, kilometraje, tiempoAnteriorServicio, estadoLlantas, garantia, tipoTransmision, tipoFrenos, numeroPuertas, carroceria, cilindraje, numeroCilindros, tipoAceite);
+    void crearAutoGasolina(int ID,int an,int km,float tas,float el,bool gar,
+    std::string tt,std::string tf,int np,std::string carr,float cil,int nc,
+    std::string ta){
+        vehiculos[indice] = new AutoGasolina(ID, an, km, tas, el, gar, tt, tf,
+         np, carr, cil, nc, ta);
         indice++;
     };
+
         /**
      * crearVan crea un objeto Van y lo agrega al arreglo de vehiculos
      * 
-     * crea un objeto Van y lo agrega al arreglo de empleados utilizando el aributo de índice para la posición
+     * crea un objeto Van y lo agrega al arreglo de empleados utilizando
+     * el aributo de índice para la posición
      * incrementándolo después en 1.
      * 
-     * @param 
+     *@param int ID, int año, int kilometraje, float tiempo anterior
+     * servicio, float estado llantas, bool garantia,
+     * string tipo transmisión, string tipo frenos,
+     * int numero asientos, string tipo combustible
      * @return
     */
-    void crearVan(int ID, int anio, int kilometraje, float tiempoAnteriorServicio, float estadoLlantas, bool garantia, std::string tipoTransmision, std::string tipoFrenos, int numeroAsientos, std::string tipoCombustible){
-        vehiculos[indice] = new Van(ID, anio, kilometraje, tiempoAnteriorServicio, estadoLlantas, garantia, tipoTransmision, tipoFrenos, numeroAsientos, tipoCombustible);
+    void crearVan(int ID, int an, int km, float tas, float el, bool gar,
+     std::string tt, std::string tf, int na, std::string tc){
+        vehiculos[indice] = new Van(ID, an, km, tas, el, gar, tt, tf, na, tc);
         indice++;
     };
-        /**
+
+    /**
      * crearMoto crea un objeto Moto y lo agrega al arreglo de vehiculos
      * 
-     * crea un objeto Moto y lo agrega al arreglo de empleados utilizando el aributo de índice para la posición
+     * crea un objeto Moto y lo agrega al arreglo de empleados utilizando
+     * el aributo de índice para la posición
      * incrementándolo después en 1.
      * 
-     * @param 
+     * @param int ID, int año, int kilometraje, float tiempo anterior
+     * servicio, float estado llantas, bool garantia,
+     * string tipo transmisión, string tipo frenos, int cilindraje
      * @return
     */
-    void crearMoto(int ID, int anio, int kilometraje, float tiempoAnteriorServicio, float estadoLlantas, bool garantia, std::string tipoTransmision, std::string tipoFrenos, int cilindrada){
-        vehiculos[indice] = new Moto(ID, anio, kilometraje, tiempoAnteriorServicio, estadoLlantas, garantia, tipoTransmision, tipoFrenos, cilindrada);
+    void crearMoto(int ID, int an, int km, float tas, float el, bool gar,
+     std::string tt, std::string tf, int cil){
+        vehiculos[indice] = new Moto(ID, an, km, tas, el, gar, tt, tf, cil);
         indice++;
     };
+
+    /**
+     * mostrarVehiculo_byID imprime el dato del vehiculo deseado
+     * Recorre la lista de vehiculos buscando la coindicencia de ID
+     * al encontrar el valor, si no se encuentra ningún vehiculo con
+     * el ID proporcionado imprime el aviso que el vehiculo no existe
+     */
     void mostrarVehiculo_byID(int ID){
+        bool found = false;
         for(unsigned int i = 0; i < indice;i++){
             if(vehiculos[i]->getID() == ID){
                 vehiculos[i]->mostrarDatos();
+                found = true;
             }
         }
+        if(!found){
+            std::cout<<"El Vehículo con ID "<<ID<<" no existe";
+        }
     };
+
+    /**
+     * mostrarFecha imprime la fecha en la que se creó el objeto
+     * Transforma a un formato compatible con cout el atributo de fecha
+     * para imprimirlo
+     * @param
+     * @return
+     */
     void mostrarFecha(){
         std::time_t fech = std::chrono::system_clock::to_time_t(this -> fecha);
         std::cout<<"Fecha Servicio: "<<std::ctime(& fech);
     };
+
+    /**
+     * updatePrecio calcula el precio total del servicio
+     * Calcula el precio total de los Vehículos dentro del objeto
+     * para actualizar el atributo de precio
+     * @param
+     * @return
+     */
     void updatePrecio(){
         float precioTotal = 0;
         for(unsigned int i = 0; i < indice; i++){
@@ -117,13 +198,29 @@ class Servicio {
         }
         this -> precio = precioTotal;
     };
+
+    /**
+     * updateDuracion calcula la duración total del servicio
+     * Calcular la duración total de los Vehículos dentro del objeto
+     * para actualizar el atributo de duración
+     * @param
+     * @return
+     */
     void updateDuracion(){
         float duracionTotal = 0;
         for(unsigned int i = 0; i < indice; i++){
             duracionTotal += vehiculos[i]->duracionServicio();
         }
-        this ->  duracion = duracionTotal;
+        this ->  duracion = duracionTotal/numeroEmpleados;
     };
+
+    /**
+     * imprimirDatos imprime los datos del objeto
+     * Recupera los atributos del objeto y los imprime
+     * junto con sus etiquetas
+     * @param
+     * @return
+     */
     void imprimirDatos(){
         updateDuracion();
         updatePrecio();
